bd3f764cf159e8b43af49c7e475e6d8ffe75187b
Onur Mehemmed <onurmamedov1@gmail.com>
2024-12-29 15:09:21 +0400

    Bezi deyisiklikler

--- LoginFormExmaple/obj/Debug/net8.0-windows/HospitalManagementSystem.AssemblyInfo.cs
+++ LoginFormExmaple/obj/Debug/net8.0-windows/HospitalManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb10bf7fb072e640fe9c2d9b2bf23464b18bb96a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
